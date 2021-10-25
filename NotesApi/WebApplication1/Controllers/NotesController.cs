5786592f205bc5aa88380d5e809e00076e2678c4
Beatris <heresbeatriselena@gmail.com>
2021-10-25 21:22:29 +0300

    NotesAPI
    Create a description and summaries of status codes for WeatherForecastController.cs that should appear in swagger documentation.

---
+++ NotesApi/WebApplication1/Controllers/NotesController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NotesApi.Controllers
+{
+    [ApiController]
+    [Route(template: "[controller]")]
+    public class NotesController : ControllerBase
+    {
+        public NotesController() 
+        { 
+
+        }
+
+        /// <summary>
+        /// Returns a list of notes.
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Note controller works");
+        }
+
+    }
+}
