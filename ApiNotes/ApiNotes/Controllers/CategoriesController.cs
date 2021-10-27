0be6e423d1c14d085cca8e0ab651ba202085eb68
Beatris <heresbeatriselena@gmail.com>
2021-10-27 08:45:35 +0300

    Api Part 2

---
+++ ApiNotes/ApiNotes/Controllers/CategoriesController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiNotes.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CategoriesController : ControllerBase
+    {
+
+        List<string> myList = new List<string> { "To do", "Done", "Doing" };
+
+        public CategoriesController() { }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <response code="400">Bad request</response>
+        /// <returns></returns>
+
+        //[HttpGet("{id}")]
+        //[Route("/categories")]
+        //public IActionResult GetWithParams(string id, string id2, string id3)
+        //{
+        //    return Ok($"id: {id} id2: {id2} id3: {id3}");
+        //}
+
+        /// <summary>
+        /// Returns a list of categories.
+        /// </summary>
+        /// <response code="400">Bad request</response>
+        /// <returns></returns>
+        [HttpGet("")]
+        public IActionResult Get()
+        {
+            return Ok(myList);
+        }
+
+        /// <summary>
+        /// Returns the filter collection.
+        /// </summary>
+        /// <response code="400">Bad request</response>
+        /// <returns></returns>
+        [HttpPost("")]
+        public IActionResult Post([FromBody] string bodyContent)
+        {
+            myList.Add(bodyContent);
+            return Created(" ",bodyContent);
+        }
+
+        /// <summary>
+        /// Returns the note collection.
+        /// </summary>
+        /// <response code="400">Bad request</response>
+        /// <returns></returns>
+        [HttpDelete("{id}")]
+        public IActionResult Delete(string id)
+        {
+            myList.Remove(id);
+            return NoContent();
+        }
+    }
+}
---
+++ ApiNotes/ApiNotes/Controllers/WeatherForecastController.cs
@@ -0,0 +1,93 @@
+﻿using ApiNotes;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NotesApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        ///  <response code="200"> Returns weather</response>
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        ///  <response code="202"> Status accepted</response>
+        ///  
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status202Accepted)]
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        ///  <response code="204"> Status no contet</response>
+        ///  
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        ///  <response code="404"> Status no found</response>
+        ///  
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        ///  <response code="205"> Reset content</response>
+        ///  
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status205ResetContent)]
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        ///  <response code="205"> Reset content</response>
+        ///  
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status205ResetContent)]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+    }
+}
